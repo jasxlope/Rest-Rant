30bfb9dde44994a338aef8705c72ac19a43f1c94
Jaslin Lopez <jasxlope@gmail.com>
2024-04-08 20:30:32 -0700

    Adding styles to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -1,10 +1,39 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {}
+function home () {
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
+}
 
 module.exports = home
 
-<a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+const React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+                /* BOOTSTRAP CDN LINK (long) */
+                <link rel="stylesheet" href="/css/style.css"/>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
